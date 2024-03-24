1f2ed05ec875e3c50c79c60e3e9f3fddbf3e0da1
tofayeLL <ahmedtofayel132@gmail.com>
2024-03-24 23:24:04 +0600

    added loading spinner from react awesome components

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,33 +1,55 @@
 import { useEffect, useState } from "react";
 import Job from "../Job/Job";
+import { RotatingLines } from 'react-loader-spinner'
 
 const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([]);
     const [dataLength, setDataLength] = useState(4);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
         fetch('jobs.json')
-        .then(res => res.json())
-        .then(data => setJobs(data));
+            .then(res => res.json())
+            .then(data => setJobs(data));
+        setLoading(false);
 
     }, [])
 
-   
+
 
 
     return (
         <div>
+            <div className="">
+                {
+                    loading ?
+                        <div className="flex justify-center items-center "> <RotatingLines
+                            visible={true}
+                            height="300"
+                            width="400"
+                            color="grey"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            ariaLabel="rotating-lines-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                        /></div> :
+                        ''
+                }
+            </div>
+
+
             <div className="flex flex-col justify-center items-center space-y-4">
                 <h1 className="text-4xl font-bold">Featured Jobs: {jobs.length}</h1>
                 <p className="font-medium">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className="grid grid-cols-2 gap-6  rounded-lg">
                 {
-                    jobs.slice(0, dataLength).map((job, index) => <Job 
-                    job={job}
-                    key={index}></Job>)
+                    jobs.slice(0, dataLength).map((job, index) => <Job
+                        job={job}
+                        key={index}></Job>)
                 }
 
             </div>
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,14 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
-
+import { RotatingLines } from 'react-loader-spinner'
 const Root = () => {
+
+    const navigation = useNavigation();
     return (
         <div >
             <div className="container mx-auto lg:px-20 px-2 my-10">
-            <Header></Header>
-            <Outlet></Outlet>
+                <Header></Header>
+
+                {
+                    navigation.state === "loading" ?
+                        <div className="flex flex-col justify-center items-center">
+                            <RotatingLines
+                                visible={true}
+                                height="300"
+                                width="500"
+                                color="grey"
+                                strokeWidth="5"
+                                animationDuration="0.75"
+                                ariaLabel="rotating-lines-loading"
+                                wrapperStyle={{}}
+                                wrapperClass=""
+                            />
+
+                        </div> :
+                        <Outlet></Outlet>
+
+                }
+
+
             </div>
             <div className="mt-32">
                 <Footer></Footer>
